b057a41ab483921959265f50ca041f2b8b9f03d0
iesley-ferreira <iesley_ferreira@hotmail.com>
2024-06-04 18:14:07 -0300

    create footer

--- FRONTEND/src/pages/Home/index.tsx
+++ FRONTEND/src/pages/Home/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import BannerCarousel from '../../components/Layout/BannerCarousel'
 import Products from '../../components/UI/Products/Products'
 
 const Home: React.FC = () => {
   return (
     <>
-      {/* <BannerCarousel /> */}
-      <Products />
+      <div className="mt-3">
+        <BannerCarousel />
+      </div>
+      <div className="m-3">
+        <Products />
+      </div>
     </>
   )
 }
