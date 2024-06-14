b6d20f4783d689b387c015d915893cac708c4e7a
Seu nome <yago_bohmcruz@hotmail.com>
2024-06-14 13:06:10 -0300

    Elimina a redundância de data, Ajusta o return, Ajusta o data de products

--- BACKEND/src/common/refresh_token_init.ts
+++ BACKEND/src/common/refresh_token_init.ts
@@ -1,5 +1,5 @@
 import bling_request from "../api/bling.request";
-import authBlingModel from "../app/models/authBling.model"
+import authBlingModel from "../app/models/authBling.model";
 import scheduleUtils from "../utils/scheduler.utils";
 import { AuthBlingUpdateType } from "../types/AuthBling.type";
 import { env } from "../env";
@@ -7,28 +7,41 @@ import cache from "../cache";
 
 const now = new Date();
 
-const credentials = Buffer.from(`${env.CLIENT_ID}:${env.CLIENT_SECRET}`).toString("base64");
+const credentials = Buffer.from(
+  `${env.CLIENT_ID}:${env.CLIENT_SECRET}`,
+).toString("base64");
 
 const refresh_token_init = async () => {
   const authBling = await authBlingModel.getAuthBling();
 
   if (!authBling) {
     const newToken = await set_token(credentials);
-    const timeScheduleMs = scheduleUtils.timeSchedule(newToken.expires_in, newToken.updated_at);
+    const timeScheduleMs = scheduleUtils.timeSchedule(
+      newToken.expires_in,
+      newToken.updated_at,
+    );
 
-    cache.blingToken.set(newToken.access_token)
+    cache.blingToken.set(newToken.access_token);
 
-    return scheduleUtils.scheduleTime(timeScheduleMs, () => refresh_token(newToken.id, credentials, newToken.refresh_token));
+    return scheduleUtils.scheduleTime(timeScheduleMs, () =>
+      refresh_token(newToken.id, credentials, newToken.refresh_token),
+    );
   }
 
   cache.blingToken.set(authBling.access_token);
 
-  const timeScheduleMs = scheduleUtils.timeSchedule(authBling.expires_in, authBling.updated_at);
-  scheduleUtils.scheduleTime(timeScheduleMs, () => refresh_token(authBling.id, credentials, authBling.refresh_token));
-}
+  const timeScheduleMs = scheduleUtils.timeSchedule(
+    authBling.expires_in,
+    authBling.updated_at,
+  );
+  scheduleUtils.scheduleTime(timeScheduleMs, () =>
+    refresh_token(authBling.id, credentials, authBling.refresh_token),
+  );
+};
 
-const set_token = async ( credentials: string ) => {
-  const { data: token_data }: { data: AuthBlingUpdateType } = await bling_request.getToken(credentials, env.AUTHORIZATION_CODE);
+const set_token = async (credentials: string) => {
+  const { data: token_data }: { data: AuthBlingUpdateType } =
+    await bling_request.getToken(credentials, env.AUTHORIZATION_CODE);
 
   const newAuth = await authBlingModel.createAuthBling({
     access_token: token_data.access_token,
@@ -38,24 +51,34 @@ const set_token = async ( credentials: string ) => {
     token_type: token_data.token_type,
   });
 
-  return newAuth
-}
+  return newAuth;
+};
 
-
-const refresh_token = async (id: string, credentials: string, refresh_token_value: string) => {
-  const { data: newTokenData }: { data: AuthBlingUpdateType } = await bling_request.refreshToken(credentials, refresh_token_value);
+const refresh_token = async (
+  id: string,
+  credentials: string,
+  refresh_token_value: string,
+) => {
+  const { data: newTokenData }: { data: AuthBlingUpdateType } =
+    await bling_request.refreshToken(credentials, refresh_token_value);
 
   const authBlingUpdated = await authBlingModel.updateAuthBling(id, {
-    ...newTokenData
+    ...newTokenData,
   });
 
   cache.blingToken.set(authBlingUpdated.access_token);
 
-  const timeScheduleMs = scheduleUtils.timeSchedule(authBlingUpdated.expires_in, authBlingUpdated.created_at);
-  scheduleUtils.scheduleTime(timeScheduleMs, () => refresh_token(authBlingUpdated.id, credentials, authBlingUpdated.refresh_token));
-}
+  const timeScheduleMs = scheduleUtils.timeSchedule(
+    authBlingUpdated.expires_in,
+    authBlingUpdated.created_at,
+  );
+  scheduleUtils.scheduleTime(timeScheduleMs, () =>
+    refresh_token(
+      authBlingUpdated.id,
+      credentials,
+      authBlingUpdated.refresh_token,
+    ),
+  );
+};
 
-
-export {
-  refresh_token_init,
-}
+export { refresh_token_init };
