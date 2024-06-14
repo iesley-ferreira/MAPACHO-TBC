ef600b97c6d680251ec36c60fae797e297efcd5c
Seu nome <yago_bohmcruz@hotmail.com>
2024-06-14 13:12:35 -0300

    Ajusta o data de Product para receber um array

--- FRONTEND/src/components/UI/Products/Products.tsx
+++ FRONTEND/src/components/UI/Products/Products.tsx
@@ -3,16 +3,17 @@ import { IProduct } from '../../../interfaces/Product'
 import ProductCard from '../ProductCard'
 
 interface ProductsProps {
-  products: IProduct[]
+  products: { data: IProduct[] }
 }
 
 const Products: React.FC<ProductsProps> = ({ products }) => {
+  const productArray = products.data
   return (
     <section className="py-12 md:pt-24 md:pb-32">
       <div className="container mx-auto px-4">
         <div className="max-w-sm md:max-w-xl mx-auto xl:max-w-none">
           <div className="flex flex-wrap -mb-6 -mx-4">
-            {products.map((product) => (
+            {productArray.map((product) => (
               <ProductCard key={product.id} product={product} />
             ))}
           </div>
