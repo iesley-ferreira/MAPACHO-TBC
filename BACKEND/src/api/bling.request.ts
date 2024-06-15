e08b860c9b3347f673406cc7df09690c3f872a7f
Seu nome <yago_bohmcruz@hotmail.com>
2024-06-15 16:22:58 -0300

    Adiciona settings.json, Ajusta o back-end

--- BACKEND/src/api/bling.request.ts
+++ BACKEND/src/api/bling.request.ts
@@ -1,55 +1,106 @@
 import { QueryType } from "../types/Query.type";
-import axiosRequest from './requests.axios';
+import axiosRequest from "./requests.axios";
 
-const BASE_URL = 'https://www.bling.com.br'
+const BASE_URL = "https://www.bling.com.br";
 
 type BlingRequestTypes = {
-  url: '/Api/v3/oauth/token' | '/Api/v3/produtos' | '/Api/v3/produtos/variacoes' | string
-  credentials?: string,
-  method: 'post' | 'get' | 'delete' | 'put'
-  data?: {
-    grant_type: "authorization_code",
-    code: string,
-  } | {
-    grant_type: "refresh_token",
-    refresh_token: string,
-  }
-  token?: string
-  query?: QueryType
-}
+  url:
+    | "/Api/v3/oauth/token"
+    | "/Api/v3/produtos"
+    | "/Api/v3/produtos/variacoes"
+    | string;
+  credentials?: string;
+  method: "post" | "get" | "delete" | "put";
+  data?:
+    | {
+        grant_type: "authorization_code";
+        code: string;
+      }
+    | {
+        grant_type: "refresh_token";
+        refresh_token: string;
+      };
+  token?: string;
+  query?: QueryType;
+};
 
-const blingRequestAxios = async ({ url,  credentials, data, token, method, query, }: BlingRequestTypes) => axiosRequest({
-  BASE_URL,
-  method,
+const blingRequestAxios = async ({
   url,
-  headers: {
-    "Content-Type": "application/x-www-form-urlencoded",
-    "Authorization": token ? `Bearer ${token}` : `Basic ${credentials}`,
-    "Accept": "application/json",
-  },
+  credentials,
   data,
+  token,
+  method,
   query,
-});
-
+}: BlingRequestTypes) =>
+  axiosRequest({
+    BASE_URL,
+    method,
+    url,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: token ? `Bearer ${token}` : `Basic ${credentials}`,
+      Accept: "application/json",
+    },
+    data,
+    query,
+  });
 
 // ================== BLING_TOKEN =============================== //
 
-const getToken = async (credentials: string, AUTHORIZATION_CODE: string) => blingRequestAxios({ url: "/Api/v3/oauth/token", method: 'post', credentials, data: { code: AUTHORIZATION_CODE, grant_type: 'authorization_code' } })
-const refreshToken = async (credentials: string, refresh_token: string) => blingRequestAxios({ url: "/Api/v3/oauth/token", method: 'post', credentials, data: { refresh_token, grant_type: "refresh_token" } })
+const getToken = async (credentials: string, AUTHORIZATION_CODE: string) =>
+  blingRequestAxios({
+    url: "/Api/v3/oauth/token",
+    method: "post",
+    credentials,
+    data: { code: AUTHORIZATION_CODE, grant_type: "authorization_code" },
+  });
+const refreshToken = async (credentials: string, refresh_token: string) =>
+  blingRequestAxios({
+    url: "/Api/v3/oauth/token",
+    method: "post",
+    credentials,
+    data: { refresh_token, grant_type: "refresh_token" },
+  });
 
 // ================== BLING_PRODUCTS =========================== //
 
-const getAllProducts = async (token: string, query?: QueryType) => blingRequestAxios({ url: "/Api/v3/produtos", method: 'get', token, query });
+const getAllProducts = async (token: string, query?: QueryType) =>
+  blingRequestAxios({
+    url: "/Api/v3/produtos",
+    method: "get",
+    token,
+    query: { criterio: "1", tipo: "P" },
+  });
+
+// ================== BLING_PRODUCTS_CATEGORY_ID =========================== //
+
+const getProductsByCategoryId = async (token: string, categoryId: string) => {
+  return blingRequestAxios({
+    url: `/Api/v3/produtos`,
+    method: "get",
+    token,
+    query: { criterio: "1", tipo: "P", idCategoria: categoryId },
+  });
+};
 
 // ================== BLING_PRODUCTS_VARIATIONS =========================== //
 const getProductsByVariation = async (token: string, fatherProduct: string) => {
-  return blingRequestAxios({ url: `/Api/v3/produtos/${fatherProduct}`, method: "get", token });
-} 
+  return blingRequestAxios({
+    url: `Api/v3/produtos/${fatherProduct}`,
+    method: "get",
+    token,
+  });
+};
 
 // ================== BLING_CATEGORIES =========================== //
 
-const getAllCategories = async (token: string, query?: QueryType) => blingRequestAxios({ url: "/Api/v3/categorias/produtos", method: 'get', token, query });
-
+const getAllCategories = async (token: string, query?: QueryType) =>
+  blingRequestAxios({
+    url: "/Api/v3/categorias/produtos",
+    method: "get",
+    token,
+    query,
+  });
 
 // ================== BLING_REQUEST =========================== //
 
@@ -59,7 +110,7 @@ const bling_request = {
   getAllProducts,
   getProductsByVariation,
   getAllCategories,
-}
-
+  getProductsByCategoryId,
+};
 
 export default bling_request;
--- BACKEND/src/app/controllers/products.controller.ts
+++ BACKEND/src/app/controllers/products.controller.ts
@@ -4,35 +4,65 @@ import productsService from "../services/products.service";
 
 const getAllProducts = async (req: Request, res: Response) => {
   try {
-    
     const bling_token = cache.blingToken.get();
-    
-    const { data, status } = await productsService.getAllProducts(bling_token, req.query);
-    
-    res.status(status).json(data.data)
+
+    const { data, status } = await productsService.getAllProducts(
+      bling_token,
+      req.query,
+    );
+
+    res.status(status).json(data.data);
   } catch (error) {
-    res.status(500).json({ error: 'An error occurred while fetching products' });
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching products" });
   }
-}
+};
+
+const getProductsByCategoryId = async (req: Request, res: Response) => {
+  console.log("ENTROU AQUI");
+
+  try {
+    const bling_token = cache.blingToken.get();
+    const { categoryId } = req.params;
+
+    const { data, status } = await productsService.getProductsByCategoryId(
+      bling_token,
+      categoryId,
+    );
+
+    console.log(" DATA AQUI", data);
+
+    res.status(status).json(data.data);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching products" });
+  }
+};
 
 const getProductByVariation = async (req: Request, res: Response) => {
   try {
-    
     const bling_token = cache.blingToken.get();
     const { idProduct } = req.params;
-    
-    const { data, status } = await productsService.getProductByVariation(bling_token, idProduct);
-    
+
+    const { data, status } = await productsService.getProductByVariation(
+      bling_token,
+      idProduct,
+    );
+
     return res.status(status).json(data);
   } catch {
-    return res.status(500).json({ error: 'An error occurred while fetching product' });
-  
+    return res
+      .status(500)
+      .json({ error: "An error occurred while fetching product" });
   }
-}
+};
 
 const productsController = {
   getAllProducts,
   getProductByVariation,
-}
+  getProductsByCategoryId,
+};
 
 export default productsController;
