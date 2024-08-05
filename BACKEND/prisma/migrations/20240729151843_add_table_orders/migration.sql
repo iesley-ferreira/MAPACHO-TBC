cbc7d6bb3eca6459635903605c2aa2f573d25fd3
Seu nome <yago_bohmcruz@hotmail.com>
2024-08-05 13:57:38 -0300

    MIGRATION BUG :bug:

---
+++ BACKEND/prisma/migrations/20240729151843_add_table_orders/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `payment_id` on the `Order` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Order" DROP COLUMN "payment_id";
