0e83aed7958208503012dd3916b44dd2a33c3f95
iesley ferreira <iesley_ferreira@hotmail.com>
2024-08-27 20:22:32 -0300

    finalizes implementation of the payment gateway and checks items in stock and updates cart if necessary / remaining

--- FRONTEND/src/store/ducks/cart/reducer.ts
+++ FRONTEND/src/store/ducks/cart/reducer.ts
@@ -1,17 +1,18 @@
-import { createReducer } from 'typesafe-actions'
-import * as actions from './actions'
-import { CartActions, CartState } from './types'
+import { createReducer } from 'typesafe-actions';
+import { ICartItem } from '../../../interfaces/Cart';
+import * as actions from './actions';
+import { CartActions, CartState } from './types';
 
 const initialState: CartState = {
   items: [],
   loading: false,
   error: false,
-}
+};
 
 const cartReducer = createReducer<CartState, CartActions>(initialState)
   .handleAction(actions.addProductToCart, (state, action) => {
-    const { product, quantidade } = action.payload
-    const existingProduct = state.items.find((item) => item.id === product.id)
+    const { product, quantidade } = action.payload;
+    const existingProduct = state.items.find((item) => item.id === product.id);
 
     if (existingProduct) {
       return {
@@ -19,37 +20,33 @@ const cartReducer = createReducer<CartState, CartActions>(initialState)
         items: state.items.map((item) =>
           item.id === product.id
             ? { ...item, quantidade: item.quantidade + quantidade }
-            : item
+            : item,
         ),
-      }
+      };
     }
 
     return {
       ...state,
       items: [...state.items, { ...product, quantidade }],
-    }
+    };
   })
   .handleAction(actions.incrementProductQuantity, (state, action) => ({
     ...state,
     items: state.items.map((item) =>
-      item.id === action.payload
-        ? { ...item, quantidade: item.quantidade + 1 }
-        : item
+      item.id === action.payload ? { ...item, quantidade: item.quantidade + 1 } : item,
     ),
   }))
   .handleAction(actions.decrementProductQuantity, (state, action) => {
     const updatedItems = state.items
       .map((item) =>
-        item.id === action.payload
-          ? { ...item, quantidade: item.quantidade - 1 }
-          : item
+        item.id === action.payload ? { ...item, quantidade: item.quantidade - 1 } : item,
       )
-      .filter((item) => item.quantidade > 0)
+      .filter((item) => item.quantidade > 0);
 
     return {
       ...state,
       items: updatedItems,
-    }
+    };
   })
   .handleAction(actions.removeProductFromCart, (state, action) => ({
     ...state,
@@ -71,5 +68,28 @@ const cartReducer = createReducer<CartState, CartActions>(initialState)
     loading: false,
     error: true,
   }))
+  .handleAction(actions.adjustCartProducts, (state, action) => {
+    const updatedCart = state.items.reduce<ICartItem[]>((acc, cartItem) => {
+      if (cartItem.id.toString() === action.payload.id.toString()) {
+        if (action.payload.availableQuantity === 0) {
+          return acc;
+        }
+        acc.push({
+          ...cartItem,
+          quantidade: action.payload.availableQuantity,
+        });
+        return acc;
+      }
+      acc.push(cartItem);
+      return acc;
+    }, []);
 
-export default cartReducer
+    localStorage.setItem('cart', JSON.stringify(updatedCart));
+
+    return {
+      ...state,
+      items: updatedCart,
+    };
+  });
+
+export default cartReducer;
